3fe867a06cac16865f26306f44223dd196fe8835
Efras2785 <efrasma@gmail.com>
2026-07-08 19:30:40 -0600

    Haciendo la conexion de los contenedores

--- sistema_escolar/config/settings.py
+++ sistema_escolar/config/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mi_clave',
+        'HOST': 'base-datos-proyecto',
+        'PORT': '5432',
     }
 }
 
